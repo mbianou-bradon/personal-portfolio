408ed127cdfb840b7767fc31604fce385a11582f
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-05-29 16:59:57 +0100

    Updated work

--- Frontend/src/components/portfolio/Portfolio.jsx
+++ Frontend/src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import "./portfolio.css";
 
 const Portfolio = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
 
-export default Portfolio
+      <div className="container portfolio__container">
+        {data.map((item) => (
+          <article className="portfolio__item" key={item.id}>
+            <div className="portfolio__item-image">
+              <img src={item.image} alt="" />
+            </div>
+            <h3>{item.title}</h3>
+            <div className="portfolio__item-cta">
+              <a href={item.github} className="btn" target="_blank" rel="noreferrer">
+                Github
+              </a>
+              <a href={item.demo} className="btn btn-primary" target="_blank" rel="noreferrer">
+                Live Demo
+              </a>
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
