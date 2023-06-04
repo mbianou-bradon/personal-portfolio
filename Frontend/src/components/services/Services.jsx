5ad85b72fb64d5ca2787a62164dd9d9be9b4e784
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-06-04 10:59:29 +0100

    Completed Service section

--- Frontend/src/components/services/Services.jsx
+++ Frontend/src/components/services/Services.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./services.css";
 import { BiCheck } from "react-icons/bi";
 import client from '../../api/axios';
+import { FadeLoader } from 'react-spinners';
 
 const Services = () => {
   const [serviceData, setServiceData] = React.useState([]);
@@ -28,58 +29,37 @@ const Services = () => {
       <h5>What I can offer</h5>
       <h2>Sevices</h2>
       <div className="services__container">
-        <article className="service">
-          <div className="service__head">
-            <h3>FrontEnd Development</h3>
-          </div>
-          <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Build Responsive web applications with React Js</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Build responsive websites with HTML, CSS and JavaScript</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Build responsive and sustainable mobile, crossplatform
-                applications with React Native
-              </p>
-            </li>
-          </ul>
-        </article>
-        <article className="service">
-          <div className="service__head">
-            <h3>BackEnd Development</h3>
-          </div>
-          <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Build stable APIs with ExpressJS and NodeJS</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Integrate various APIs such as Email, payment, Messaging</p>
-            </li>
-          </ul>
-        </article>
-        <article className="service">
-          <div className="service__head">
-            <h3>Database Administration</h3>
-          </div>
-          <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Manage NONSQL databases such as Firebase and MongoDB</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Manage SQL databases such MySQL, PostgressSQL</p>
-            </li>
-          </ul>
-        </article>
+        {
+          isLoading? 
+          <>
+          <FadeLoader color="#4db5ff" />
+          </>
+
+          :
+          serviceData.map((service, index)=> {
+            return (
+              <article className="service" key={index}>
+                <div className="service__head">
+                  <h3>{service.title}</h3>
+                </div>
+                <ul className="service__list">
+                  {
+                    service.description.map((desc, index)=>{
+                      return (
+                        <li key={index}>
+                          <BiCheck className="service__list-icon" />
+                          <p>{desc}</p>
+                        </li>
+                      )
+                    })
+                  }
+                </ul>
+              </article>
+            )
+          })
+          
+        }
+        
       </div>
     </section>
 
