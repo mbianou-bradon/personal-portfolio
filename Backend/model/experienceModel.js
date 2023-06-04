9056dee65de36e79c2887acb17a817f98ebecc86
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-06-04 08:41:23 +0100

    Installed spinner

--- Backend/model/experienceModel.js
+++ Backend/model/experienceModel.js
@@ -1,27 +1,25 @@
 const mongoose = require("mongoose");
 
 const experienceSchema = new mongoose.Schema({
-    Category : {
-        title : {
-            type: String,
-            required : [true, "Category title required"],
-            unique: [true, "Category title must be unique"]
-        },
-        experiences : [
-            skill = {
-                title : {
-                    type: String,
-                    required : [true, "an experience must have a title"],
-                    unique : [true, "An experience title must be unique, please a different title"]
-                },
-                skillLevel: {
-                    type: String,
-                    required : [true, "an experience must have a skill level"]
-                }
-            }
-        ]
-    }
     
+    title : {
+        type: String,
+        required : [true, "Category title required"],
+        unique: [true, "Category title must be unique"]
+    },
+    experiences : [
+        {
+            title : {
+                type: String,
+                required : [true, "an experience must have a title"],
+                unique : [true, "An experience title must be unique, please a different title"]
+            },
+            skillLevel: {
+                type: String,
+                required : [true, "an experience must have a skill level"]
+            }
+        }
+    ]   
     
 })
 
